a5b4858e89bbd70f0219da7c53f0aac925f700d7
Tim Andersen <timander@gmail.com>
2015-02-24 16:51:29 +0000

    upgraded hibernate & utilized @Transactional annotation

--- web/src/main/java/net/timandersen/repository/RaffleRepository.java
+++ web/src/main/java/net/timandersen/repository/RaffleRepository.java
@@ -17,25 +17,19 @@ public class RaffleRepository {
     private SessionFactory sessionFactory;
 
     public List<Raffle> findAll() {
-        sessionFactory.getCurrentSession().beginTransaction();
         Query query = sessionFactory.getCurrentSession().createQuery("from Raffle");
         return (List<Raffle>) query.list();
     }
 
     public void save(Raffle entity) {
-        sessionFactory.getCurrentSession().beginTransaction();
         sessionFactory.getCurrentSession().save(entity);
-        sessionFactory.getCurrentSession().getTransaction().commit();
     }
 
     public void delete(Raffle entity) {
-        sessionFactory.getCurrentSession().beginTransaction();
         sessionFactory.getCurrentSession().delete(entity);
-        sessionFactory.getCurrentSession().getTransaction().commit();
     }
 
     public Raffle findById(Integer raffleId) {
-        sessionFactory.getCurrentSession().beginTransaction();
         Query query = sessionFactory.getCurrentSession().createQuery("from Raffle where id = :id");
         query.setInteger("id", raffleId);
         List<Raffle> list = query.list();
